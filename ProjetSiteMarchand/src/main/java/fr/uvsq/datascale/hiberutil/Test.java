1162b1c8b298325650e42f7988713cb4cd19cb3f
André <Andr@DESKTOP-G4SG9UK>
2017-01-23 21:15:12 +0100

    reparation

--- ProjetSiteMarchand/src/main/java/fr/uvsq/datascale/hiberutil/Test.java
+++ ProjetSiteMarchand/src/main/java/fr/uvsq/datascale/hiberutil/Test.java
@@ -1,24 +1,21 @@
 package fr.uvsq.datascale.hiberutil;
 
-
 import fr.uvsq.datascale.dao.ClientDAO;
 import fr.uvsq.datascale.modele.Client;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		Client cl = new Client () ;
-		
-		cl.setNom("aaa");
-		
-		ClientDAO clDAO = new ClientDAO() ;
-		
-		clDAO.insert(cl);
-		
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
 
-	}
+	Client cl = new Client();
+
+	cl.setNom("aaa");
+
+	ClientDAO clDAO = new ClientDAO();
+
+	clDAO.insert(cl);
+
+    }
 
 }
