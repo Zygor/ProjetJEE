b5ad6214ee31dd21464c40b14d2374f11ecf81e1
MIGAOU <idriss.migaou@gmail.com>
2017-01-23 16:59:35 +0100

    t

---
+++ ProjetSiteMarchand/src/main/java/fr/uvsq/datascale/hiberutil/Test.java
@@ -0,0 +1,24 @@
+package fr.uvsq.datascale.hiberutil;
+
+
+import fr.uvsq.datascale.dao.ClientDAO;
+import fr.uvsq.datasclale.modele.Client;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Client cl = new Client () ;
+		
+		cl.setNom("aaa");
+		
+		ClientDAO clDAO = new ClientDAO() ;
+		
+		clDAO.insert(cl);
+		
+
+	}
+
+}
